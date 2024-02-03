d4e7721fcb4232c51cd5fd1f65213865b12d2a1a
RaminMajidi <raminmajididev01@gmail.com>
2024-02-03 21:26:02 +0330

    create the basic structure

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Button from '@mui/material/Button';
+
+function App() {
+
+  return (
+    <Button
+      style={{ margin: '20px' }}
+      color='error'
+      variant="contained"
+      
+      >
+      Hello world
+    </Button>
+  )
+}
+
+export default App
