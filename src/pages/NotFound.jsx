19f7211f79af4b56a811976d484d8d6c0f9360ed
RaminMajidi <raminmajididev01@gmail.com>
2024-02-17 22:52:09 +0330

    create the NotFound Page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,8 +1,50 @@
-import React from 'react'
+import Box from "@mui/material/Box"
+import Typography from '@mui/material/Typography'
+import img from "../assets/images/404.png"
+import Button from '@mui/material/Button'
+import { Link } from "react-router-dom"
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
+    <Box sx={{}}>
+      <Typography
+        my={"1rem"}
+        fontWeight={"bold"}
+        textAlign={"center"}
+        variant="h2"
+        color="initial">
+        404
+      </Typography>
+      <Typography
+        my={"1rem"}
+        fontWeight={"bold"}
+        textAlign={"center"}
+        variant="h4"
+        color="initial">
+        Page Not Found !
+      </Typography>
+
+      <img
+        style={{
+          width: "95%", maxWidth: "300px",
+          margin: "auto", display: "block",
+        }}
+        src={img}
+        alt="Not Found Page Image"
+      />
+
+      <Link to={"/"}>
+        <Button
+          variant="contained"
+          color="primary"
+          sx={{ mt: "2rem", mx: "auto", display: "block" }}
+        >
+          Go To Home
+        </Button>
+      </Link>
+
+
+    </Box>
   )
 }
 
