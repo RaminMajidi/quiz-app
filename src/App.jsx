2a187c49e13e677e80eb7f8dcd8d8c46d7e8e8b4
RaminMajidi <raminmajididev01@gmail.com>
2024-02-22 14:56:25 +0330

    create the gh-pages configs

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
     <>
       <ToastContainer />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/"  element={<Home />} />
         <Route path="/quiz" element={<Quiz />} />
         <Route path="/result" element={<Result />} />
         <Route path="/404" element={<NotFound />} />
